ba3c28848bc553c4f10711b42534f94c49976705
Vidura Dissanayaka <vidurad@millenniumitesp.com>
2026-07-13 16:04:10 +0530

    Add Lombock Annotation

--- src/main/java/com/millenniumitesp/productinventoryservice/config/StockLimitsProperties.java
+++ src/main/java/com/millenniumitesp/productinventoryservice/config/StockLimitsProperties.java
@@ -1,15 +1,15 @@
 package com.millenniumitesp.productinventoryservice.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Setter
+@Getter
 @ConfigurationProperties(prefix = "inventory.stock")
 public class StockLimitsProperties {
 
     private int minLimit;
     private int maxLimit;
 
-    public int getMinLimit() { return minLimit; }
-    public void setMinLimit(int minLimit) { this.minLimit = minLimit; }
-    public int getMaxLimit() { return maxLimit; }
-    public void setMaxLimit(int maxLimit) { this.maxLimit = maxLimit; }
 }
