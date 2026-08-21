1be067983048664b140000a820748cae83e473cd
Vidura Dissanayaka <vidurad@millenniumitesp.com>
2026-08-21 12:25:42 +0530

    Initialize CorsConfig and modify Security config.

--- src/main/java/com/millenniumitesp/productinventoryservice/config/SecurityConfig.java
+++ src/main/java/com/millenniumitesp/productinventoryservice/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.millenniumitesp.productinventoryservice.security.JwtAuthenticationEnt
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,6 +36,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint(authenticationEntryPoint)
